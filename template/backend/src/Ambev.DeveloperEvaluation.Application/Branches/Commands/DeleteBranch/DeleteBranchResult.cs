c33596357e52b0676bf27770a8f067a62a0553a3
Lauro H <laurohen@hotmail.com>
2024-12-14 10:23:08 -0300

    features branches - adjusts validation/controller/handlers/entity

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Branches/Commands/DeleteBranch/DeleteBranchResult.cs
@@ -0,0 +1,20 @@
+﻿using Ambev.DeveloperEvaluation.Common.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Application.Branches.Commands.Delete_Branch
+{
+    public class DeleteBranchResult
+    {
+        /// <summary>
+        /// Indicates whether the deletion was successful
+        /// </summary>
+        public bool Success { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+        public List<ValidationErrorDetail>? Errors { get; set; }
+    }
+}
--- template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Branches/DeleteBranch/DeleteBranchResponse.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Branches/DeleteBranch/DeleteBranchResponse.cs
@@ -1,8 +1,11 @@
-﻿namespace Ambev.DeveloperEvaluation.WebApi.Features.Branches.DeleteBranch
+﻿using Ambev.DeveloperEvaluation.Common.Validation;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Branches.DeleteBranch
 {
     public class DeleteBranchResponse
     {
         public bool Success { get; set; }
         public string Message { get; set; } = string.Empty;
+        public List<ValidationErrorDetail>? Errors { get; set; }
     }
 }
